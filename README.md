cc0bec70d0e1f2a8feaac7bf359c0154304da358
Shaba Abhiram <shabarivas.abhiram@gmail.com>
2015-02-02 09:44:48 -0800

    temp update about to rename repo

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # rpi-connect-to-wifi
 
-A Node application which makes connecting to your home wifi easier
+A Node application which makes connecting your RaspberryPi to your home wifi easier
 
 ## Why?
 
@@ -19,7 +19,14 @@ $npm start
 
 ## Usage
 
-Ideally this service runs on the PIs startup. It essentilly checks the currently configured wifi connection, and on failure switches to AP mode. 
+This is approximately what occurs when we run this app:
+
+1. Check to see if we are connected to a wifi AP
+2. If connected to a wifi, do nothing -> exit
+3. Convert RPI to act as a AP (with a configurable SSID)
+4. Host a lightweight HTTP server which allows for the user to connect and configure the RPIs wifi connection. The interfaces exposed are RESTy so other applications can similarly implement their own UIs around the data returned.
+5. Once the RPI is successfully configured, reset it to act as a wifi device (not AP anymore), and setup its wifi network based on what the user picked.
+6. At this stage, the RPI is named, and has a valid wifi connection which its bound to, reboot the pi and re-run this script on startup.
 
 ## Testing
 
--- server.js
+++ server.js
@@ -1,1 +1,22 @@
-console.log("Hello Node");
+var _                   = require("underscore")._,
+    async               = require("async"),
+
+    util                = require("util"),
+    fs                  = require("fs"),
+    path                = require("path");
+
+/*****************************************************************************\
+    1. Check to see if we are connected to a wifi AP
+    2. If connected to a wifi, do nothing -> exit
+    3. Convert RPI to act as a AP (with a configurable SSID)
+    4. Host a lightweight HTTP server which allows for the user to connect and
+       configure the RPIs wifi connection. The interfaces exposed are RESTy so
+       other applications can similarly implement their own UIs around the
+       data returned.
+    5. Once the RPI is successfully configured, reset it to act as a wifi
+       device (not AP anymore), and setup its wifi network based on what the
+       user picked.
+    6. At this stage, the RPI is named, and has a valid wifi connection which
+       its bound to, reboot the pi and re-run this script on startup.
+\*****************************************************************************/
+

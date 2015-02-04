11529f0de352e6f752d4a9ca85d1f732c9829246
Shaba Abhiram <shabarivas.abhiram@gmail.com>
2015-02-03 19:11:52 -0800

    Add scan result selection

--- app/public/app.js
+++ app/public/app.js
@@ -21,15 +21,25 @@ app.controller("AppController", ["PiManager", "$scope", "$location", "$timeout",
     function(PiManager, $scope, $location, $timeout) {
         // Scope variable declaration
         $scope.scan_results = [];
+        $scope.selected_index = null;
 
         // Scope function definitions
         $scope.rescan = function() {
             $scope.scan_results = [];
+            $scope.selected_index = null;
             PiManager.rescan_wifi().then(function(response) {
+                console.log(response.data);
                 if (response.data.status == "SUCCESS") {
                     $scope.scan_results = response.data.scan_results;
                 }
             });
+        }
+
+        $scope.change_selection = function(index) {
+            console.log("Change selection to: " + index);
+            if (index >= 0 && index < $scope.scan_results.length) {
+                $scope.selected_index = index;
+            }
         }
 
         // Defer load the scanned results from the rpi

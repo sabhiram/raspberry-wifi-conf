120bdcea4f1c61eb2710ddee825a11eb906bf08e
hoostine <hoostine@gmail.com>
2023-10-19 21:37:47 -0500

    ...

--- provision.sh
+++ provision.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
+echo Be patient, this is slow and as silent as I could make it...
 apt-get update -qq
 apt-get upgrade -qq
 apt-get install -qq dnsmasq hostapd iw dhcpcd nodejs npm
 npm update --quiet
 npm install bower -g --quiet
-bower install
+bower install --allow-root
 apt remove --purge -qq NetworkManager
